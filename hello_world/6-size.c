c3d6f495b323e1207dee1a1be480ed79186d9e98
Matias Davezac <8675@holbertonstudents.com>
2024-02-14 17:12:13 +0000

    acg

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,6 +3,15 @@
 /**
 * main - asdfg
 *
+*
+*
+*
+*
+*
+*
+*
+*
+*
 * Return: 0 if success
 */
 int main(void)
@@ -12,9 +21,10 @@ int main(void)
 	long int var_3;
 	long long int var_4;
 	float var_5;
+
 	printf("Size of a char: %lu byte(s)", sizeof(var_1));
 	printf("\nSize of an int: %lu byte(s)", sizeof(var_2));
-	printf("\nSize of a long int: %lu byte(s)",sizeof(var_3));
+	printf("\nSize of a long int: %lu byte(s)", sizeof(var_3));
 	printf("\nSize of a long long int: %lu byte(s)", sizeof(var_4));
 	printf("\nSize of a float: %lu byte(s)", sizeof(var_5));
 	putchar('\n');
