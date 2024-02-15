ef9dd8c3e9290201d227f74415693aa97727b330
Matias Davezac <8675@holbertonstudents.com>
2024-02-15 14:42:44 +0000

    acg

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - description
+*
+* Return: description
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
