9c57c298897a15246c8cb1b529cc0ca2be4a7377
Matias Davezac <8675@holbertonstudents.com>
2024-02-15 14:45:58 +0000

    acg

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - description
+*
+* Return: description
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
