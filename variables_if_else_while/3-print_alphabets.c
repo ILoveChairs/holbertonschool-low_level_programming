4a420bed545690d736891a44e77cc48552ebcee7
Matias Davezac <8675@holbertonstudents.com>
2024-02-15 14:29:12 +0000

    acg

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - description
+*
+* Return: description
+*/
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i <= 122; i++)
+                putchar(i);	
+	for (i = 65; i <= 90; i++)
+                putchar(i);
+	putchar('\n');
+	return (0);
+}
