54c1d19ea60957c3ef496b4cad953781b603bd18
Matias Davezac <8675@holbertonstudents.com>
2024-02-15 12:51:28 +0000

    acg

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,10 @@
 */
 int main(void)
 {
-	srand(time(0));
-	int n = rand() - RAND_MAX / 2;
+	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
