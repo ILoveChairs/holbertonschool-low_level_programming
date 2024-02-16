966f9bd7482e9d9e227b8b645648acdf780c0e90
Matias Davezac <8675@holbertonstudents.com>
2024-02-16 17:35:41 +0000

    acg

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * print_last_digit - asdfg
+ * @n: asdf
+ *
+ * Return: asdfg
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
