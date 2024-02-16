6e0bb49cffee8343ed481d13c7e96dda80e45f3f
Matias Davezac <8675@holbertonstudents.com>
2024-02-16 17:32:22 +0000

    acg

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * _abs - asdfg
+ * @n: asdf
+ *
+ * Return: asdfg
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (n * -1);
+}
