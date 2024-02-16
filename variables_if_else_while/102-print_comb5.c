dc5c8367fe8c8bc00f2a860f8f4f2db799ef0fb0
Matias Davezac <8675@holbertonstudents.com>
2024-02-16 13:15:35 +0000

    acg

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -5,52 +5,77 @@
 *
 * Return: description
 */
-int main(void)
+
+void lfun(int *i, int *j, int *k, int *lt, int *kf, int *lf)
+{
+        int l;
+
+        for (l = *lt; l <= 57; l++)
+        {
+                putchar(*i);
+                putchar(*j);
+                putchar(' ');
+                putchar(*k);
+                putchar(l);
+                if (*i == 57 && *j == 56)
+                        break;
+                putchar(',');
+                putchar(' ');
+        }
+        *lt = 48;
+}
+
+void kfun(int *i, int *j, int *kf, int *lf)
+{
+        int k;
+        int lt;
+
+        lt = *lf;
+        if (*lf == 57)
+        {
+                if (*kf != 57)
+                        *kf++;
+                *lf = 48;
+        }
+        else
+        {
+                *lf++;
+        }
+        if (*i == 57 && *j == 57)
+                *kf = 100;
+        for (k = *kf; k<= 57; k++)
+                lfun(i, j, &k, &lt, kf, lf);
+}
+
+void jfun(int *i, int *kf, int *lf)
+{
+        int j;
+
+        for (j = 48; j <= 57; j++)
+        {
+                kfun(i, &j, kf, lf);
+        }
+}
+
+void ifun(int *kf, int *lf)
 {
 	int i;
-	int j;
-	int k;
-	int l;
-	int lt;
+
+	for (i = 48; i<57; i++)
+		jfun(&i, kf, lf);
+}
+
+int main(void)
+{
 	int kf;
 	int lf;
 
 	kf = 48;
 	lf = 49;
-	for (i = 48; i <= 57; i++)
-	{
-		for (j = 48; j <= 56; j++)
-		{
-			lt = lf;
-			for (k = kf; k <= 57; k++)
-			{
-				for (l = lt; l <= 57; l++)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(k);
-					putchar(l);
-					if (i != 57 || j != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				lt = 48;
-			}
-		}
-		if (lf == 57)
-		{
-			kf++;
-			putchar('M');
-			lf = 48;
-		}
-		else
-		{
-			lf++;
-		}
-	}
+	
+	ifun(&kf, &lf);
+
 	putchar('\n');
+	
 	return (0);
 }
