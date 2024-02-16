8c622fe8e497d1dadef2df8220b6acf0b33c0c3f
Matias Davezac <8675@holbertonstudents.com>
2024-02-16 16:48:45 +0000

    acg

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * print_alphabet_x10 - asdfg
+ *
+ * Description: asdfg
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; i != 10; i++)
+		for (c = 97; c != 123; c++)
+			_putchar(c);
+	_putchar('\n');
+}
