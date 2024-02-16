16fc552e5b1019f4fe20d39b540cc9d1380c9283
Matias Davezac <8675@holbertonstudents.com>
2024-02-16 17:15:25 +0000

    acg

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * print_sign - asdfg
+ * @n: asdf
+ * 
+ * Return: asdfg
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0);
+}
