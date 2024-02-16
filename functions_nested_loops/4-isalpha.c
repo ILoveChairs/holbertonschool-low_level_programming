cbd8b0e7f6010010dc9c9cf802d3a6d61a94d04f
Matias Davezac <8675@holbertonstudents.com>
2024-02-16 17:10:54 +0000

    acg

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * _isalpha - asdfg
+ *
+ * Return: asdfg
+ */
+int _isalpha(int c)
+{
+	if ((65 <= c && c <= 90) || (97 <= c && c <= 122))
+		return (1);
+	return (0);
+}
