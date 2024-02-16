e31afc3a4cd0343e795d235f56e5a95af14bd081
Matias Davezac <8675@holbertonstudents.com>
2024-02-16 17:04:26 +0000

    acg

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ *  - asdfg
+ *
+ * Return: asdfg
+ */
+int _islower(int c)
+{
+	if (c >= 97)
+		return (1);
+	return (0);
+}
