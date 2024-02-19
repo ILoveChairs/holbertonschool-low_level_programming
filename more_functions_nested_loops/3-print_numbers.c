035dcc803c05dd4d801f53a3b5c477d200b54ea9
Matias Davezac <8675@holbertonstudents.com>
2024-02-19 17:03:06 +0000

    acg

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * print_numbers - asdfg
+ * 
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar(10);
+}
