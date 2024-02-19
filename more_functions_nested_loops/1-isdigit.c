cba5b92fa2a5a9a033d14a8c1fda684f4630b0b4
Matias Davezac <8675@holbertonstudents.com>
2024-02-19 16:56:47 +0000

    acg

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * _isdigit - asdfg
+ * @c: asdf
+ *
+ * Return: asdfg
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
