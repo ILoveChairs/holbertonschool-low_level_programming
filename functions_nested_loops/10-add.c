6a5339b05da20a22acb3cd957f131afa657b7837
Matias Davezac <8675@holbertonstudents.com>
2024-02-19 13:01:08 +0000

    acg

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * add - asdfg
+ * @x: asdfg
+ * @y: asdfg
+ *
+ * Return: asdfg
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
