1a76d30f6783c1597a3c2dab3ce92b6d1942f585
Matias Davezac <8675@holbertonstudents.com>
2024-02-19 16:59:22 +0000

    acg

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * mul - asdfg
+ * @a: asdf
+ * @b: asdf
+ *
+ * Return: asdfg
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
