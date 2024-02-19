e1565757a4ea38b2c8c8d83650096afdc738c936
Matias Davezac <8675@holbertonstudents.com>
2024-02-19 18:33:25 +0000

    acg

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * print_triangle - asdfg
+ * @size
+ */
+void print_triangle(int size)
+{
+	int x;
+	int y;
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (x >= size - y - 1)
+				_putchar(35);
+			else
+				_putchar(32);
+		}
+		_putchar(10);
+	}
+	if (size == 0)
+		_putchar(10);
+}
