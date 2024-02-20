c3e0df053f0fcd9eb741412924169d6a35cc78bb
Matias Davezac <8675@holbertonstudents.com>
2024-02-20 18:14:10 +0000

    acg

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -18,12 +18,12 @@ int main(void)
 	b = 2.0;
 
 	printf("1, 2, ");
-	for (i = 0; i <= 94; i++)
+	for (i = 0; i <= 95; i++)
 	{
 		tmp = b;
 		b = b + a;
 		a = tmp;
-		if (i != 94)
+		if (i != 95)
 			printf("%.0Lf, ", b);
 		else
 			printf("%.0Lf\n", b);
