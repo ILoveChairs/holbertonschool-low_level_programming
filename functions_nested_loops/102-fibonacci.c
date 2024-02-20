4d341522fbb7960fad847c75a1b7a9801a45e4d9
Matias Davezac <8675@holbertonstudents.com>
2024-02-20 14:34:08 +0000

    acg

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -15,12 +15,12 @@ int main(void)
 	a = 1;
 	b = 2;
 	printf("1, 2, ");
-	for (i = 0; i <= 50; i++)
+	for (i = 0; i <= 47; i++)
 	{
 		tmp = b;
 		b = b + a;
 		a = tmp;
-		if (i != 50)
+		if (i != 47)
 			printf("%lu, ", b);
 		else
 			printf("%lu\n", b);
