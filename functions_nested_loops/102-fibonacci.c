bfb3e4556dc7bd465add690ce709449539567d54
Matias Davezac <8675@holbertonstudents.com>
2024-02-20 14:31:29 +0000

    acg

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+/**
+ * main - asdfg
+ *
+ * Return: asdfg
+ */
+int main(void)
+{
+	long unsigned int i;
+	long unsigned int a;
+	long unsigned int b;
+	long unsigned int tmp;
+
+	a = 1;
+	b = 2;
+	for (i = 0; i <= 50; i++)
+	{
+		tmp = b;
+		b = b + a;
+		a = tmp;
+		if (i != 50)
+			printf("%lu, ", b);
+		else
+			printf("%lu\n", b);
+	}
+	return (0);
+}
