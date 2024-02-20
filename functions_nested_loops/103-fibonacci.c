837627f34f1a9002d151529ec42491dd70db1b31
Matias Davezac <8675@holbertonstudents.com>
2024-02-20 15:00:15 +0000

    acg

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -7,23 +7,23 @@
  */
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int a;
-	unsigned long int b;
-	unsigned long int tmp;
-	unsigned long int sum;
+	long int i;
+	long int a;
+	long int b;
+	long int tmp;
+	long int sum;
 
 	a = 1;
 	b = 2;
 	sum = 2;
-	for (i = 1; b <= 4000000; i++)
+	for (i = 1; b < 4000000; i++)
 	{
 		tmp = b;
 		b = b + a;
 		a = tmp;
-		if (i % 2 == 0)
+		if (b % 2 == 0)
 			sum = sum + b;
 	}
-	printf("%lu\n", sum);
+	printf("%ld\n", sum);
 	return (0);
 }
