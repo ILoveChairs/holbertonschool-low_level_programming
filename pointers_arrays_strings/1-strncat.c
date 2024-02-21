1bfe669911217f3068b1c6d0755c5603f257333b
Matias Davezac <8675@holbertonstudents.com>
2024-02-21 17:08:33 +0000

    acg

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -4,11 +4,25 @@
 #endif
 
 /**
- *  - asdfg
- *
+ * _strncat - asdfg
+ * @dest: sad
+ * @src: sad
+ * @n: sad
  * Return: asdfg
  */
- (void)
+char *_strncat(char *dest, char *src, int n)
 {
-	return (0);
+	int i;
+	int l;
+
+	for (i = 0; *dest; dest++)
+		i = i + 1;
+	for (l = 0; *src && l < n; src++)
+	{
+		*dest = *src;
+		dest = dest + 1;
+		i = i + 1;
+		l = l + 1;
+	}
+	return (dest - i);
 }
