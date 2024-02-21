66c67f2788672bcb90e29f877c34fccdf84ff8b9
Matias Davezac <8675@holbertonstudents.com>
2024-02-21 18:31:25 +0000

    acg

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,11 +4,41 @@
 #endif
 
 /**
- *  - asdfg
- *
+ * _strncpy - asdfg
+ * @dest: asdfg
+ * @src: asdfg
+ * @n: asdfg
  * Return: asdfg
  */
- (void)
+char *_strncpy(char *dest, char *src, int n)
 {
-	return (0);
+	int i;
+	int l;
+	char foo;
+	char bar;
+	char tmp[1024];
+	int booler = 1;
+
+	i = 0;
+	for (l = 0; *src && l < n; src++)
+		tmp[l] = *src;
+
+	for (x = 0; x <= n; x++)
+	{
+		
+		for (bar = 2; booler; dest++)
+		{
+			foo = *dest;
+			*dest = bar;
+			bar = foo;
+			i += 1;
+		}
+		dest = dest - i + 1;
+	}
+	dest = dest - i;
+	for (i = 0; *dest; dest++)
+	{
+		i = i + 1;
+	}
+	return (dest - i);
 }
