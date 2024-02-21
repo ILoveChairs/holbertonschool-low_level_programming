3eaaf108961c818c959572252255735b6def6ca6
Matias Davezac <8675@holbertonstudents.com>
2024-02-21 13:39:15 +0000

    acg

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,11 +4,25 @@
 #endif
 
 /**
- *  - asdfg
+ * print_rev - asdfg
  *
  * Return: asdfg
  */
- (void)
+void print_rev(char *s)
 {
-	return (0);
+	int i = 0;
+
+	while (*s)
+	{
+		s = s + 1;
+		i = i + 1;
+	}
+	s = s - 1;
+	while (i != 0)
+	{
+		_putchar(*s);
+		s = s - 1;
+		i = i - 1;
+	}
+	_putchar(10);
 }
