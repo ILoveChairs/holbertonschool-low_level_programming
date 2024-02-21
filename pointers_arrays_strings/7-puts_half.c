8c75351076cfbe6311867eb6e35bc38f805aced4
Matias Davezac <8675@holbertonstudents.com>
2024-02-21 14:09:31 +0000

    acg

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,11 +4,17 @@
 #endif
 
 /**
- *  - asdfg
- *
- * Return: asdfg
+ * puts_half - asdfg
+ * @str: asdfg
  */
- (void)
+void puts_half(char *str)
 {
-	return (0);
+	int len;
+
+	for (len = 0; *str; len++)
+		str = str + 1;
+	str = str - len / 2;
+	for (; *str; str++)
+		_putchar(*str);
+	_putchar(10);
 }
