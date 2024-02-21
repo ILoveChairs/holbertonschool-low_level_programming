d5bf4646adb3d7e33a90361710745aff58c7f8a2
Matias Davezac <8675@holbertonstudents.com>
2024-02-21 15:56:47 +0000

    acg

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,49 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include "main.h"
+#include <math.h>
 #endif
 
 /**
- *  - asdfg
- *
+ * _atoi - asdfg
+ * @s: asdfg
  * Return: asdfg
  */
- (void)
+int _atoi(char *s)
 {
-	return (0);
+	int num;
+	int num_bool;
+	int nega;
+	int nega_bool;
+
+	num = 0;
+	num_bool = 0;
+	nega = 0;
+	nega_bool = 1;
+	for (; *s; s++)
+	{
+		if (*s == '-' && nega_bool)
+			nega = nega - 1;
+		else if (*s == '+' && nega_bool)
+			nega = nega + 1;
+		else if (*s >= 48 && *s <= 57)
+		{
+			if (num_bool || num == 0)
+			{
+				num_bool = 1;
+				if (num > 0)
+					num = (num * 10) + (*s - 48);
+				else
+					num = *s - 48;
+			}
+		}
+		else if (num_bool)
+		{
+			num_bool = 0;
+			nega_bool = 0;
+		}
+	}
+	if (nega < 0)
+		return (num * -1);
+	return (num);
 }
