d7ef601ce9aa8aa53f728dd67d51f48e4370d755
Matias Davezac <8675@holbertonstudents.com>
2024-02-22 13:07:53 +0000

    acg

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,11 +4,21 @@
 #endif
 
 /**
- *  - asdfg
- *
+ * _strcmp - asdfg
+ * @s1: asdfg
+ * @s2: asdfg
  * Return: asdfg
  */
- (void)
+int _strcmp(char *s1, char *s2)
 {
+	int i;
+
+	for (i = 0; *s1; s1++)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s2 += 1;
+		i += 1;
+	}
 	return (0);
 }
