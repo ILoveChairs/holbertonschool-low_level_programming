368be84607fd61516a2b0a656835b9dc4d38ea25
Matias Davezac <8675@holbertonstudents.com>
2024-02-22 14:27:03 +0000

    acg

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,11 +4,51 @@
 #endif
 
 /**
- *  - asdfg
- *
+  * 
+  *
+  */
+int _check_if_separator(char c)
+{
+	if (c == 9 || c == 10 || c == 32)
+		return (1);
+	else if (c == 33 || c == 34 || c == 40)
+		return (1);
+	else if (c == 41 || c == 44 || c == 46)
+		return (1);
+	else if (c == 59 || c == 63 || c == 123)
+		return (1);
+	else if (c == 125)
+		return (1);
+	return (0);
+}
+
+/**
+ * cap_string - asdfg
+ * @c: asdfg
  * Return: asdfg
  */
- (void)
+char *cap_string(char *c)
 {
-	return (0);
+	int i;
+	int new_bool;
+
+	new_bool = 1;
+	for (i = 0; *c; c++)
+	{
+		char cc = *c;
+		if ((cc >= 65 && cc) || (cc >= 97 && cc <= 122))
+		{
+			if (new_bool)
+			{
+				if (cc >= 97 && cc <= 122)
+					*c = *c - 32;
+				new_bool = 0;
+			}
+		}
+		else if (_check_if_separator(cc))
+			new_bool = 1;
+		i += 1;
+	}
+
+	return (c - i);
 }
