8e222ed8e90f692411aa75d31ba2fd7bd23f0c40
Matias Davezac <8675@holbertonstudents.com>
2024-02-22 16:11:55 +0000

    acg

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,11 +4,26 @@
 #endif
 
 /**
- *  - asdfg
- *
+ * leet - asdfg
+ * @n: asdfg
  * Return: asdfg
  */
- (void)
+char *leet(char *n)
 {
-	return (0);
+	int i;
+	int l;
+	char to_replace[] = {65, 69, 79, 84, 76};
+	char leet_l[] = {52, 51, 48, 55, 49};
+
+	for (i = 0; *n; n++)
+	{
+		for (l = 0; l < 5; l++)
+		{
+			if ((*n == to_replace[l]) || (*n == to_replace[l] + 32))
+				*n = leet_l[l];
+		}
+		i += 1;
+	}
+
+	return (n - i);
 }
