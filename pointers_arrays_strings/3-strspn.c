c4265b64ad3cea682e8253491da98ce5d15492c6
Matias Davezac <8675@holbertonstudents.com>
2024-02-23 12:28:37 +0000

    acg

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,5 +11,29 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (0);
+	int l;
+	int len;
+	int i_bool;
+	int l_bool;
+
+	len = 0;
+	i_bool = 1;
+	for (; *s; s++)
+	{
+		if (i_bool)
+		{
+			l_bool = 1;
+			for (l = 0; accept[l]; l++)
+			{
+				if (*s == accept[l])
+				{
+					len += 1;
+					l_bool = 0;
+				}
+			}
+			if (l_bool && len > 0)
+				i_bool = 0;
+		}
+	}
+	return (len);
 }
