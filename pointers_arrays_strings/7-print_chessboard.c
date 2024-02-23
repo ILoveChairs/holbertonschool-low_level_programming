fb960daa73c3cebfb5c1313e52b2962757a31b8e
Matias Davezac <8675@holbertonstudents.com>
2024-02-23 14:29:10 +0000

    acg

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,5 +9,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	return (0);
+	int i;
+	int l;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (l = 0; l < 8; l++)
+		{
+			_putchar(a[i][l]);
+		}
+		_putchar(10);
+	}
 }
