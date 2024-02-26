d2f03ecf9dfb4675b7b5d160caa701fa9e64dfe9
Matias Davezac <8675@holbertonstudents.com>
2024-02-26 17:34:03 +0000

    acg

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,19 +4,34 @@
 #endif
 
 /**
+ * is_divisible - asdfg
+ * @n: asdfg
+ * @d: asdfg
+ *
+ * Return: asdfg
+ */
+int is_divisible(int n, int d)
+{
+	if (n % d == 0)
+		return (0);
+	if (d == n / 2)
+		return (1);
+	return (is_divisible(n, d + 1));
+}
+
+/**
  * is_prime_number - asdfg
  * @n: asdfg
+ *
  * Return: asdfg
  */
 int is_prime_number(int n)
 {
-	int x;
+	int d = 2;
 
-	if (n == 1)
-		return (2);
-	if ()
-	{
-
-	}
-	return (0);
+	if (n <= 1)
+		return (0);
+	if (n >= 2 && n <= 3)
+		return (1);
+	return (is_divisible(n, d));
 }
