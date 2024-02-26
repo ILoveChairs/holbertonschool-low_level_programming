6b3128db3398a62a3a81aa87206cd5a8e353c218
Matias Davezac <8675@holbertonstudents.com>
2024-02-26 17:27:55 +0000

    acg

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,29 +4,35 @@
 #endif
 
 /**
+ * _sqrt - asdfg
+ * @n: asdfg
+ * @r: asdfg
+ *
+ * Return: asdfg
+ */
+int _sqrt(int n, int r)
+{
+	if ((r * r) == n)
+		return (r);
+	if (r == n / 2)
+		return (-1);
+	return (_sqrt(n, r + 1));
+}
+
+/**
  * _sqrt_recursion - asdfg
  * @n: asdfg
+ *
  * Return: asdfg
  */
 int _sqrt_recursion(int n)
 {
+	int r;
+
+	r = 0;
 	if (n < 0)
-	{
 		return (-1);
-	}
-
-	if (n > 99)
-	{
-		n = _sqrt_recursion(n);
-	}
-	else
-	{
-		
-	}
-
-	if (n > 0)
-	{
-		return (n);
-	}
-	return (-1);
+	if (n == 1)
+		return (1);
+	return (_sqrt(n, r));
 }
