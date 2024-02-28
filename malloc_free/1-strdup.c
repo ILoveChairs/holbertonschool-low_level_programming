c95887380552570264d70dc43ae995a62cab7618
Matias Davezac <8675@holbertonstudents.com>
2024-02-28 16:03:07 +0000

    acg

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,5 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 #endif
 
@@ -10,5 +12,20 @@
  */
 char *_strdup(char *str)
 {
-	return (0);
+	char *cpy;
+	int i;
+	int len;
+
+	len = 0;
+	for (i = 0; str[i]; i++)
+		len++;
+
+	cpy = malloc(sizeof(char) * len + 1);
+	if (cpy == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		cpy[i] = str[i];
+
+	return (cpy);
 }
