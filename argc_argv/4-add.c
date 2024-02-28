981ded41d24d53576e5793748671c651fdcf01a0
Matias Davezac <8675@holbertonstudents.com>
2024-02-28 13:22:34 +0000

    acg

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - asdfg
@@ -8,6 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
-	
+	int i;
+	int out;
+
+	if (argc < 3)
+	{
+		puts("Error");
+		return (1);
+	}
+	out = 0;
+	for (i = 1; i < argc; i++)
+	{
+		out += atoi(argv[i]);
+	}
+
+	printf("%d\n", out);
 	return (0);
 }
