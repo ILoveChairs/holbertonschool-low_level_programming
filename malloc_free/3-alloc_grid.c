7bddcd893633112f202d024322c605994e5f8045
Matias Davezac <8675@holbertonstudents.com>
2024-02-28 17:30:58 +0000

    acg

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	
 	arr = (int**)malloc(height * sizeof(int*));
-	for (i = 0; i < height; i++)
+	for (i = 0; i < width; i++)
 		arr[i] = (int*)malloc(width * sizeof(int));
 
 	if (!arr)
