25c17123071246678e10c33b392b9422f0419b5e
Matias Davezac <8675@holbertonstudents.com>
2024-02-28 16:30:38 +0000

    acg

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdlib.h>
 #include "main.h"
 #endif
 
@@ -11,5 +12,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	return (0);
+	char *s;
+	int len1;
+	int len2;
+	int i;
+
+	len1 = 0;
+	for (i = 0; s1[i]; i++)
+		len1++;
+	len2 = 0;
+	for (i = 0; s2[i]; i++)
+		len2++;
+
+	s = malloc(sizeof(char) * len1 + sizeof(char) * len2);
+
+	if (s1)
+	{
+		for (i = 0; i < len1; i++)
+			s[i] = s1[i];
+	}
+	if (s2)
+	{
+		for (i = 0; i < len2; i++)
+			s[len1 + i] = s2[i];
+	}
+
+	s[len1 + len2 + 1] = '\0';
+
+	return (s);
 }
