06a07a46613907f62cfe9709322da73a805371f6
Matias Davezac <8675@holbertonstudents.com>
2024-02-28 15:49:25 +0000

    acg

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 
-	s = malloc(sizeof(char) * size + 1);
+	s = malloc(sizeof(char) * size);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
