ef2338cbf6cd85f15cbcf831ff407fe76fe48620
Matias Davezac <8675@holbertonstudents.com>
2024-02-28 15:45:51 +0000

    acg

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdlib.h>
 #include "main.h"
 #endif
 
@@ -12,10 +13,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
+	unsigned int i;
 
 	if (size == 0)
 		return (0);
 
-	s = malloc();
+	s = malloc(sizeof(char) * size + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
 	return (s);
 }
