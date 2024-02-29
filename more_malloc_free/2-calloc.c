f6ea1c45d72a5faea291c03cd6099ef39894a74b
Matias Davezac <8675@holbertonstudents.com>
2024-02-29 17:40:08 +0000

    acg

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,14 +1,34 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdlib.h>
 #include "main.h"
 #endif
 
 /**
- *  - asdfg
- *
+ * _calloc - asdfg
+ * @nmemb: asdfg
+ * @size: asdfg
  * Return: asdfg
  */
- (void)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	return (0);
+	char *arr;
+	int i;
+	int len;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	len = nmemb * size;
+	arr = malloc(len);
+
+	if (!arr)
+		return (NULL);
+
+	for (i = 0; i < len + 1; i++)
+	{
+		arr[i] = '\0';
+	}
+
+	return (arr);
 }
