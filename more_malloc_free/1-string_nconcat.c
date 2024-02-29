e10fee508d79c844adacf11d75a012129f84461c
Matias Davezac <8675@holbertonstudents.com>
2024-02-29 16:00:01 +0000

    acg

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,14 +1,52 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 #endif
 
 /**
- *  - asdfg
- *
+ * string_nconcat - asdfg
+ * @s1: asdfg
+ * @s2: asdfg
+ * @n: asdfg
  * Return: asdfg
  */
- (void)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	return (0);
+	char *str;
+	unsigned int i;
+	int l;
+	int len1;
+	int len2;
+
+	len1 = 0;
+	for (i = 0; s1[i]; i++)
+		len1 += 1;
+
+	len2 = 0;
+	for (i = 0; s2[i] && i < n; i++)
+		len2 += 1;
+
+	str = malloc((len1 * sizeof(char)) + (len2 * sizeof(char)) + 1);
+
+	if (!str)
+		return (NULL);
+
+	l = 0;
+
+	for (i = 0; s1[i]; i++)
+	{
+		str[l] = s1[i];
+		l += 1;
+	}
+
+	for (i = 0; s2[i] && i < n; i++)
+	{
+		str[l] = s2[i];
+		l += 1;
+	}
+
+	str[l] = '\0';
+	return (str);
 }
