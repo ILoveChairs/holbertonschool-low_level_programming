d35cf1318cce690dcd960a1d7a2bcc89f34ff447
Matias Davezac <8675@holbertonstudents.com>
2024-03-01 12:45:17 +0000

    acg

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -20,12 +20,12 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min + 1));
+	arr = calloc(max - min + 1, sizeof(int));
 
 	if (!arr)
 		return (NULL);
 
-	for (i = 0; i <= max; i++)
+	for (i = 0; i <= max - min; i++)
 	{
 		arr[i] = i;
 	}
