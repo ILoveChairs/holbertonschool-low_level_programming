24a067ef7ef4ddec9edddb78436f97056fa7c04a
Matias Davezac <8675@holbertonstudents.com>
2024-03-01 14:38:53 +0000

    acg

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#ifndef PRRUM
+#define PRRUM
+#include "dog.h"
+#endif
+
+/**
+  * init_dog - asdfg
+  * @d: asdfg
+  * @name: asdfg
+  * @age: asdfg
+  * @owner: asdfg
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
