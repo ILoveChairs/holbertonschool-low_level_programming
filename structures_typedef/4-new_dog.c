edc14971a3f97a3bf9f3e4e2f4cb460b7b25e09a
Matias Davezac <8675@holbertonstudents.com>
2024-03-01 18:22:57 +0000

    acg

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #ifndef PRRUM
 #define PRRUM
+#include <stdlib.h>
 #include "dog.h"
 #endif
 
@@ -13,9 +14,15 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t dog;
-	dog.age = age;
-	dog.name = name;
-	dog.owner = owner;
-	return (&dog);
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+
+	if (!dog)
+		return (NULL);
+
+	dog->age = age;
+	dog->name = name;
+	dog->owner = owner;
+	return (dog);
 }
