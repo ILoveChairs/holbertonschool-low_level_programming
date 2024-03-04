5d6033e9db279bcbd70b9a527d854b2bd83b0a64
Matias Davezac <8675@holbertonstudents.com>
2024-03-04 18:38:39 +0000

    acg

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#ifndef PRRUM
+#define PRRUM
+#include "function_pointers.h"
+#endif
+
+/**
+  * int_index - asdfg
+  * @array: asdfg
+  * @size: asdfg
+  * @cmp: asdfg
+  * Return: asdfg
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
