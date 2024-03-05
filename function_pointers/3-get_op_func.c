76a47561c171154930c16997cd4793b0dbdfcead
Matias Davezac <8675@holbertonstudents.com>
2024-03-05 17:57:24 +0000

    Version 0, testing if checker likes it

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,39 @@
+#ifndef GET_OP_FUNC
+#define GET_OP_FUNC
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#endif
+
+
+/**
+  * get_op_func - asdfg
+  * @s: asdfg
+  *
+  * Return: asdfg
+  */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
+
+
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#ifndef OP_FUNCTIONS
+#define OP_FUNCTIONS
+#include "3-calc.h"
+#endif
+
+/**
+  * op_add - Adds 2 ints together
+  * @a: int 1
+  * @b: int 2
+  *
+  * Return: int (a + b)
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - Substracts 2 ints together
+  * @a: int 1
+  * @b: int 2
+  *
+  * Return: int (a - b)
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - Multiplies 2 ints together
+  * @a: int 1
+  * @b: int 2
+  *
+  * Return: int (a * b)
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - di
+  * @a: int 1
+  * @b: int 2
+  *
+  * Return: int (a / b)
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - Returns reminder of a / b
+  * @a: int 1
+  * @b: int 2
+  *
+  * Return: int (a % b)
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
