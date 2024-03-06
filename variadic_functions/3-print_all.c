a36119d694b71e465f61b5b9504eba17c8a53b0e
Matias Davezac <8675@holbertonstudents.com>
2024-03-06 18:23:29 +0000

    acg

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,67 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+
+
+/**
+ *
+ *
+ */
+void _print_char(char c)
+{
+	putchar(c);
+}
+
+/**
+ *
+ *
+ */
+void _print_str(char *s)
+{
+	printf("%s", s);
+}
+
+/**
+ *
+ *
+ */
+void _print_int(int i)
+{
+	printf("%i", i);
+}
+
+/**
+ *
+ *
+ */
+void _print_float(float f)
+{
+	printf("%f", f);
+}
+
+/**
+ *
+ *
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	void ()
+
+	va_start();
+
+	while()
+	{
+		
+	}
+
+	va_end();
+	putchar('\n');
+}
+
+
+
+
