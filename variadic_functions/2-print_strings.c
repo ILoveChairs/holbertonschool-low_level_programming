f8aafefc6be4ad31ade4f6b68892cd77743969cf
Matias Davezac <8675@holbertonstudents.com>
2024-03-06 16:12:58 +0000

    2nd excercise, betty good, output good

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _print_str - prints str
+ * @str: str
+ */
+void _print_str(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		putchar(str[i]);
+}
+
+/**
+ * print_strings - prints strings
+ * @separator: separates
+ * @n: num
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	unsigned int l;
+
+	va_start(args, n);
+
+	if (separator == NULL)
+	{
+		for (i = 0; i < n; i++)
+			_print_str(va_arg(args, char*));
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_print_str(va_arg(args, char*));
+			if (i + 1 != n)
+			{
+				for (l = 0; separator[l]; l++)
+					putchar(separator[l]);
+			}
+		}
+	}
+
+	va_end(args);
+	putchar('\n');
+}
