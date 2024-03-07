09de99059058971bbcbd286f2591a3b65ff5e24a
Matias Davezac <8675@holbertonstudents.com>
2024-03-07 14:58:31 +0000

    betty is happy, main output is correct, unsure about number of if statements

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,64 +1,93 @@
 #include <stdarg.h>
 #include <stdio.h>
-
+#include "variadic_functions.h"
 
 
 /**
- *
- *
+ * _print_char - prints character
+ * @args: argument list
  */
-void _print_char(char c)
+void _print_char(va_list args)
 {
-	putchar(c);
+	printf("%c", va_arg(args, int));
 }
 
 /**
- *
- *
+ * _print_str - pritns string
+ * @args: argument list
  */
-void _print_str(char *s)
+void _print_str(va_list args)
 {
-	printf("%s", s);
+	char *str;
+
+	str = va_arg(args, char*);
+	if (str == NULL)
+	{
+		str = "(nil)";
+	}
+	printf("%s", str);
 }
 
 /**
- *
- *
+ * _print_int - pritns int
+ * @args: argument list
  */
-void _print_int(int i)
+void _print_int(va_list args)
 {
-	printf("%i", i);
+	printf("%i", va_arg(args, int));
 }
 
 /**
- *
- *
+ * _print_float - prints float
+ * @args: argument list
  */
-void _print_float(float f)
+void _print_float(va_list args)
 {
-	printf("%f", f);
+	printf("%f", va_arg(args, double));
 }
 
 /**
- *
- *
+ * print_all - print list of arguments based
+ * @format: type of arguments on string
  *
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i;
+	unsigned int l;
 
-	void ()
+	functioner funcs[] = {
+		{"c", _print_char},
+		{"s", _print_str},
+		{"i", _print_int},
+		{"f", _print_float},
+		{NULL, NULL}
+	};
 
-	va_start();
+	va_start(args, format);
 
-	while()
+	i = 0;
+	while (format[i])
 	{
-		
+		l = 0;
+		while (funcs[l].type)
+		{
+			if (*funcs[l].type == format[i])
+			{
+				funcs[l].func(args);
+				if (format[i + 1])
+				{
+					printf(", ");
+				}
+				break;
+			}
+			l++;
+		}
+		i++;
 	}
 
-	va_end();
+	va_end(args);
 	putchar('\n');
 }
 
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,18 @@
 #ifndef __VARIADIC_FUNCTIONS
 #define __VARIADIC_FUNCTIONS
+#include <stdarg.h>
+
+/**
+ * struct functioner - matches function pointer with char
+ * @type: char
+ * @func: func
+ */
+typedef struct functioner
+{
+	char *type;
+	void (*func)(va_list args);
+} functioner;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
