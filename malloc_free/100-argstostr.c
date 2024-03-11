6876868c36c3a1cab88596a203b79fb60d04e3f6
Matias Davezac <8675@holbertonstudents.com>
2024-03-11 14:28:49 +0000

    100th task check, betty good, result good

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#ifndef I_DONT_NEED_GUARDS_BUT_I_PUT_THEM_ANYWAYS
+#define I_DONT_NEED_GUARDS_BUT_I_PUT_THEM_ANYWAYS
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#endif
+
+/**
+ * argstostr - funcion that does stuff
+ * @ac: asdfg
+ * @av: asdfg
+ *
+ * Return: asdfg
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i;
+	int l;
+	int j;
+	int len;
+
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+
+	len = 0;
+	for (i = 1; i < ac; i++)
+	{
+		for (l = 0; av[i][l]; l++)
+		{
+			len++;
+		}
+		len++;
+	}
+
+	str = malloc(len * sizeof(char));
+	if (!str)
+		return (NULL);
+
+	j = 0;
+	for (i = 1; i < ac; i++)
+	{
+		for (l = 0; av[i][l]; l++)
+		{
+			str[j] = av[i][l];
+			j++;
+		}
+		str[j] = '\n';
+		j++;
+	}
+
+	return (str);
+}
