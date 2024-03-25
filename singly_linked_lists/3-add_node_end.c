0b74346237a141de1de2bfd0bbc64e669a541d2c
Matias Davezac <8675@holbertonstudents.com>
2024-03-25 18:26:16 +0000

    Added Task Number 3 It took me a while. The problem was a while. The condition of a while. Have i mentioned i hate whiles?

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,16 +1,55 @@
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 
 /**
- *  - asdfg
+ * add_node_end - asdfg
  *
- * @: asdfg
- * @: asdfg
+ * @head: asdfg
+ * @str: asdfg
  *
  * Return: asdfg
  */
-
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *head;
+	list_t *new_node;
+	list_t *current;
+	char *string;
+	int len;
 
-	return ();
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	string = strdup(str);
+	if (!string)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = string;
+
+	for (len = 0; string[len]; len++)
+		;
+
+	new_node->len = len;
+	new_node->next = NULL;
+
+	if (!(*head))
+		*head = new_node;
+	else if (!(*head)->next)
+		(*head)->next = new_node;
+	else
+	{
+		current = *head;
+		while (current->next)
+			current = current->next;
+		current->next = new_node;
+	}
+
+	return (new_node);
+
 }
