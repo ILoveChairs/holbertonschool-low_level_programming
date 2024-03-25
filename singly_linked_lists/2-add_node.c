c087be6fdf7319d5f8a890cea0b472d89c5fdc1a
Matias Davezac <8675@holbertonstudents.com>
2024-03-25 17:14:29 +0000

    Added Task Number 2 Not exactly sure how it works though. Should review this later. I'm too tired rn.

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,16 +1,41 @@
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
- *  - asdfg
+ * add_node - asdfg
  *
- * @: asdfg
- * @: asdfg
+ * @head: asdfg
+ * @str: asdfg
  *
  * Return: asdfg
  */
-
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *head;
+	list_t *new_node;
+	char *string;
+	int len;
 
-	return ();
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	string = strdup(str);
+	if (!string)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = string;
+
+	for (len = 0; string[len]; len++)
+		;
+
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
