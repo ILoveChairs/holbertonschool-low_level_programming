1eeab09e1c6bf06fdfb0d2a77ba3e80d0cdcfca0
Matias Davezac <8675@holbertonstudents.com>
2024-03-28 20:40:11 +0000

    Added Task 3 Had trouble with naming stuff. Think this is alright.

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * add_dnodeint_end - asdfg
@@ -10,9 +11,30 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	list_t *;
+	dlistint_t *new;
+	dlistint_t *current;
 
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
 
+	new->n = n;
+	new->next = NULL;
 
-	return (*head);
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	current = *head;
+
+	while (current->next)
+		current = current->next;
+
+	new->prev = current;
+	current->next = new;
+
+	return (new);
 }
