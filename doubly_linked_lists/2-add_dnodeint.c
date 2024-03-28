00d070cd6e7f06b8e094831b9c59c4f173de3031
Matias Davezac <8675@holbertonstudents.com>
2024-03-28 20:16:32 +0000

    Added Task 2 Had problems with adding if head was null.

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- *  add_dnodeint- asdfg
+ * add_dnodeint - asdfg
  *
  * @head: asdfg
  * @n: asdfg
@@ -10,9 +11,24 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	list_t *;
+	dlistint_t *current;
 
+	current = malloc(sizeof(dlistint_t));
+	if (!current)
+		return (NULL);
 
+	current->n = n;
+	current->prev = NULL;
+	current->next = *head;
 
-	return (*head);
+	if (!*head)
+	{
+		*head = current;
+		return (current);
+	}
+
+	(*head)->prev = current;
+	*head = current;
+
+	return (current);
 }
