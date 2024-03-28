5f4ee02d4d66f44930bd8391d64064435657c416
Matias Davezac <8675@holbertonstudents.com>
2024-03-28 21:00:43 +0000

    Added Task 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  *  get_dnodeint_at_index- asdfg
@@ -10,9 +11,23 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	list_t *head;
+	dlistint_t *current;
+	unsigned int i;
 
+	if (!head)
+		return (NULL);
 
+	current = head;
+	for (i = 0; current->next; i++)
+	{
+		if (i == index)
+			return (current);
 
-	return (head);
+		current = current->next;
+	}
+
+	if (i == index)
+		return (current);
+
+	return (NULL);
 }
