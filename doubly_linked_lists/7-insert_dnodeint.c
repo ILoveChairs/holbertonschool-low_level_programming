7eff65389d48eef875c7bc20c2e286d39dd9b4a8
Matias Davezac <8675@holbertonstudents.com>
2024-03-28 22:02:03 +0000

    Added Task 7. Took me a while to get it working. betty is checked. However i'm not confident it's gonna pass all the checks.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,33 @@
 #include "lists.h"
+#include <stdlib.h>
+
+
+
+/**
+ * insert_dnodeint - asdfg
+ *
+ * @h: asdfg
+ * @n: asdfg
+ *
+ * Return: asdfg
+ */
+dlistint_t *insert_dnodeint(dlistint_t **h, int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+
+	new->n = n;
+	new->prev = *h;
+	new->next = (*h)->next;
+
+	if (!new->next)
+		(new->next)->prev = new;
+
+	(*h)->next = new;
+
+	return (new);
+}
 
 /**
  * insert_dnodeint_at_index - asdfg
@@ -11,7 +40,35 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	list_t *head;
+	dlistint_t *head;
+	dlistint_t *output;
+	unsigned int i;
 
-	return ();
+	if (n == 0)
+		return (add_dnodeint(h, n));
+
+	if (!*h)
+		return (NULL);
+
+	head = *h;
+	for (i = 0; (*h)->next; i++)
+	{
+		if (i + 1 == idx)
+		{
+			output = insert_dnodeint(h, n);
+			*h = head;
+			return (output);
+		}
+
+		*h = (*h)->next;
+	}
+
+	if (i == idx)
+	{
+		output = insert_dnodeint(h, n);
+		*h = head;
+		return (output);
+	}
+
+	return (NULL);
 }
