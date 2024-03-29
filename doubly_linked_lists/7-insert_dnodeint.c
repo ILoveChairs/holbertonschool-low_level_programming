a315bf9152ec38cfa6d915fcd4e8ad5072c6af87
Matias Davezac <8675@holbertonstudents.com>
2024-03-29 21:38:00 +0000

    Fixing task 7 bug where idx wasnt checked if 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -45,7 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *output;
 	unsigned int i;
 
-	if (n == 0)
+	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	if (!*h)
