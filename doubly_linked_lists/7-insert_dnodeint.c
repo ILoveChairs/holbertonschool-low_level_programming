3e34348e458891ceb8497e570e776dec9ab9c6d4
Matias Davezac <8675@holbertonstudents.com>
2024-03-30 23:10:24 +0000

    Fix of Task 7. Fixed last index position not getting printed. Fixed head staying in last position.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 
 
+/**
+ * dnodeint_go_head - asdfg
+ *
+ * @h: asdfg
+ *
+ * Return: asdfg
+ */
+dlistint_t *dnodeint_go_head(dlistint_t **h)
+{
+	while ((*h)->prev)
+		*h = (*h)->prev;
+	return (*h);
+}
 
 /**
  * insert_dnodeint - asdfg
@@ -16,8 +29,6 @@ dlistint_t *insert_dnodeint(dlistint_t **h, int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-
-	*h = (*h)->prev;
 
 	new->n = n;
 	new->prev = *h;
@@ -41,7 +52,6 @@ dlistint_t *insert_dnodeint(dlistint_t **h, int n)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *head;
 	dlistint_t *output;
 	unsigned int i;
 
@@ -51,25 +61,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!*h)
 		return (NULL);
 
-	head = *h;
+	output = NULL;
+
 	for (i = 0; (*h)->next; i++)
 	{
 		if (i == idx)
 		{
 			output = insert_dnodeint(h, n);
-			*h = head;
-			return (output);
 		}
 
 		*h = (*h)->next;
 	}
 
-	if (i == idx)
+	if (++i == idx)
 	{
 		output = add_dnodeint_end(h, n);
-		*h = head;
-		return (output);
 	}
 
-	return (NULL);
+	dnodeint_go_head(h);
+	return (output);
 }
