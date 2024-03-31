cff90e91ab930bd8bb27a112a217bcaf12b4454a
Matias Davezac <8675@holbertonstudents.com>
2024-03-31 02:00:55 +0000

    Fix of Task 0, now closes file if error.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,6 +4,52 @@
 #include <stdlib.h>
 
 /**
+ * _whiler - asdfg
+ *
+ * @fd: asdfg
+ * @buffer: asdfg
+ * @letters: asdfg
+ *
+ * Return: asdfg
+ */
+ssize_t _whiler(int fd, char *buffer, ssize_t letters)
+{
+	ssize_t bytes_wrote;
+	ssize_t bytes_read;
+	ssize_t wrote_total;
+
+	wrote_total = 0;
+	while (1)
+	{
+		bytes_read = read(fd, buffer, 1024);
+		if (bytes_read == -1)
+		{
+			close(fd);
+			return (0);
+		}
+
+		if (bytes_read == 0)
+			break;
+
+		if ((ssize_t)letters > wrote_total + bytes_read)
+			bytes_wrote = write(1, buffer, bytes_read);
+
+		else
+			bytes_wrote = write(1, buffer, letters - wrote_total);
+
+		if (bytes_read != bytes_wrote)
+		{
+			close(fd);
+			return (0);
+		}
+
+		wrote_total += bytes_wrote;
+	}
+
+	return (wrote_total);
+}
+
+/**
  * read_textfile - asdfg
  *
  * @filename: asdfg
@@ -14,42 +60,22 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int bytes_read;
 	char *buffer;
-	ssize_t bytes_wrote;
 	ssize_t wrote_total;
 
 	if (!filename)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(1024);
 	if (!buffer)
-		return (0);
-
-	wrote_total = 0;
-	while (1)
 	{
-		bytes_read = read(fd, buffer, 1024);
-		if (bytes_read == -1)
-			return (0);
-
-		if (bytes_read == 0)
-			break;
-
-		if ((ssize_t)letters > wrote_total + bytes_read)
-			bytes_wrote = write(1, buffer, bytes_read);
-		else
-			bytes_wrote = write(1, buffer, letters - wrote_total);
-
-		if (bytes_read != bytes_wrote)
-			return (0);
-
-		wrote_total += bytes_wrote;
+		close(fd);
+		return (0);
 	}
+
+	wrote_total = _whiler(fd, buffer, (ssize_t)letters);
 
 	close(fd);
 	free(buffer);
