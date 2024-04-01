9bff2627cf7748898167e51f039b5f1fdcc9d8ba
Matias Davezac <8675@holbertonstudents.com>
2024-04-01 14:59:21 +0000

    Added Task 2. Minecraft.

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,11 +11,21 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int i;
 
+	for (i = 0; n >> i != 0; i++)
+		;
 
+	for (; i > 0; i--)
+	{
+		if (i == index)
+			return ((n >> i) & 1);
+	}
 
+	if (i == index)
+		return ((n >> i) & 1);
 
-
+	return (-1);
 }
 
 
