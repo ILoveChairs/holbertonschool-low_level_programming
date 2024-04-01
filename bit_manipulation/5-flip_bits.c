e8ddb98bef0a7cc93325d37b49f1782f2caabfe8
Matias Davezac <8675@holbertonstudents.com>
2024-04-01 18:06:25 +0000

    Added Task 5. XAnd

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -9,13 +9,22 @@
  *
  * Return: asdfg
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int flips;
+	unsigned int i;
 
+	flips = 0;
+	for (i = 0; i <= 32; i++)
+	{
+		if (((n >> i) & 1) || ((m >> i) & 1))
+		{
+			if (!(((n >> i) & 1) && ((m >> i) & 1)))
+				flips++;
+		}
+	}
 
-
-
-
+	return (flips);
 }
 
 
