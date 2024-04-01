77de173c068c0b44fbff70cc23bdd037120148ff
Matias Davezac <8675@holbertonstudents.com>
2024-04-01 13:02:47 +0000

    Added Task 0, fixed include issue. I sure love whilessss.

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,19 @@
-#include <main.h>
+#include "main.h"
+
+/**
+ * _pow - asdfg
+ *
+ * @a: num
+ * @b: num
+ *
+ * Return: asdfg
+ */
+int _pow(int a, int b)
+{
+	if (b <= 0)
+		return (1);
+	return (a * _pow(a, b - 1));
+}
 
 /**
  * binary_to_uint - converts binary string to decimal.
@@ -9,11 +24,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int u;
+	int i;
 
+	if (!b)
+		return (0);
 
+	while (*b)
+		b++;
 
+	u = 0;
+	i = 0;
+	while (*(--b))
+	{
+		if (!(*b == '0' || *b == '1'))
+			return (0);
 
+		u += (*b - 48) * _pow(2, i++);
+	}
 
+	return (u);
 }
 
 
