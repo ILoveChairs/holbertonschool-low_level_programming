9b83b0667f57113799dc528d9e6ae3ed7f1a4521
Matias Davezac <8675@holbertonstudents.com>
2024-04-01 14:08:01 +0000

    Added Task 1. Got me a lil confused ngl.

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -7,11 +7,18 @@
  */
 void print_binary(unsigned long int n)
 {
+	int len;
 
+	for (len = 1; n >> len != 0; len++)
+		;
 
+	len--;
 
-
-
+	while (len >= 0)
+	{
+		_putchar(((n >> len) & 1) + 48);
+		len--;
+	}
 }
 
 
