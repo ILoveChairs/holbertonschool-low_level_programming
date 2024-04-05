5ffc64ea537b44abbd034f5cd507cabe4b70b5ef
Matias Davezac <8675@holbertonstudents.com>
2024-04-05 15:29:58 -0300

    Fixed T1, permissions of files should be ok now. A bit too late to be doing this.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -110,7 +110,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT|O_WRONLY|O_TRUNC, O_WRONLY);
+	fd = open(filename, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR + S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
