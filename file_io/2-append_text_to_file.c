d67cf85110c920f62ce7b3c4ab3d7541b0562b36
Matias Davezac <8675@holbertonstudents.com>
2024-04-08 09:22:37 -0300

    Added T2. Just copied T1 and modified the open attributes.

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,17 +1,131 @@
-
-
-
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *  - asdfg
+ * _str_len - asdfg
  *
- * @: asdfg
- *
- * @: asdfg
+ * @str: asdfg
  *
  * Return: asdfg
  */
-
+unsigned int _str_len(char *str)
 {
+	unsigned int i;
 
+	for (i = 0; str[i]; i++)
+		;
+
+	return (i);
+}
+
+
+/**
+ * _flush_buffer - asdfg
+ *
+ * @buffer: asdfg
+ */
+void _flush_buffer(char *buffer)
+{
+	while (*buffer)
+		*buffer++ = 0;
+}
+
+/**
+ * _fill_buffer - asdfg
+ *
+ * @buffer: asdfg
+ *
+ * @text_content: asdfg
+ *
+ * Return: asdfg
+ */
+int _fill_buffer(char *buffer, char *text_content)
+{
+	unsigned int i;
+	unsigned int size;
+
+	size = _str_len(text_content);
+	if (size > 1024)
+		size = 1024;
+
+	for (i = 0; text_content[i] && i < size; i++)
+	{
+		buffer[i] = text_content[i];
+	}
+
+	return (i);
+}
+
+/**
+ * _whiler - asdfg
+ *
+ * @fd: asdfg
+ * @buffer: asdfg
+ * @text_content: asdfg
+ *
+ * Return: asdfg
+ */
+ssize_t _whiler(int fd, char *buffer, char *text_content)
+{
+	ssize_t bytes_wrote;
+	ssize_t bytes_read;
+	ssize_t wrote_total;
+
+	wrote_total = 0;
+	while (1)
+	{
+		_flush_buffer(buffer);
+		bytes_read = _fill_buffer(buffer, text_content + wrote_total);
+
+		bytes_wrote = write(fd, buffer, bytes_read);
+
+		if (bytes_read != bytes_wrote)
+			return (-1);
+
+		wrote_total += bytes_wrote;
+
+		if (bytes_read < 1024)
+			break;
+	}
+
+	return (wrote_total);
+}
+
+/**
+ * create_file - asdfg
+ *
+ * @filename: asdfg
+ * @text_content: asdfg
+ *
+ * Return: asdfg
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	char *buffer;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	buffer = malloc(1024);
+	if (!buffer)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	if (text_content)
+		_whiler(fd, buffer, text_content);
+
+	close(fd);
+	free(buffer);
+	return (1);
 }
